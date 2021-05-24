fc45871e70ab7554bc6a008f295d5c00944fca2b
Michał Szumowski <mic.szumowski@gmail.com>
2021-05-24 15:21:00 +0200

    Add images functionality to proccesOrder method - exercise 8.7

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 /* global Handlebars, utils, dataSource */ // npmeslint-disable-line no-unused-vars
 
 {
-  // 'use strict'; // 
+  'use strict'; // 
 
   const select = {
     templateOf: {
@@ -90,6 +90,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion(){
@@ -133,11 +134,11 @@
 
     processOrder(){
       const thisProduct = this;
-      console.log('processOrder');
+      // console.log('processOrder');
 
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
+      // console.log('formData', formData);
 
       // set price to default price
       let price = thisProduct.data.price;
@@ -146,16 +147,17 @@
       for(let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
+        // console.log(paramId, param);
 
         // for every option in this category
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option, !option.default == true);
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          // console.log(optionId, option, !option.default == true);
 
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if(formData[paramId] && formData[paramId].includes(optionId)) {
+          if(optionSelected) {
             // check if the option is not default
             if(!option.default == true) {
               // add option price to price variable
@@ -168,9 +170,18 @@
               price -= option.price;
             }
           }
-        }
-      }
+          
+          const optionImage = thisProduct.imageWrapper.querySelector('.'+ paramId + '-' + optionId);
 
+          if (optionImage) {
+            if(optionSelected) {
+              optionImage.classList.add('active');
+            } else {
+              optionImage.classList.remove('active');
+            }
+          } 
+        }  
+      }
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
