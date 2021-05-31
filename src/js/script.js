f9c51d50f6de5da4cf0ba604e377af679e84b30b
Michał Szumowski <mic.szumowski@gmail.com>
2021-06-01 00:05:06 +0200

    Fixed an issue with validation

--- src/js/script.js
+++ src/js/script.js
@@ -255,13 +255,13 @@
       thisWidget.linkDecrease.addEventListener('click', function (event) {
         event.preventDefault();
 
-        thisWidget.setValue(thisWidget.value -= 1);
+        thisWidget.setValue(thisWidget.value - 1);
       });
 
       thisWidget.linkIncrease.addEventListener('click', function (event) {
         event.preventDefault();
 
-        thisWidget.setValue(thisWidget.value += 1);
+        thisWidget.setValue(thisWidget.value + 1);
       });
     }
 
